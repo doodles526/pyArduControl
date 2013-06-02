e8a7568f80ae1cdc51c3e503bc39fa867866700c
Joshua Deare <doodles@doodles.(none)>
2013-06-02 13:57:05 -0700

    modified the motor control code to track

--- __init__.py
+++ __init__.py
@@ -2,10 +2,25 @@ import pyfirmata
 from pyfirmata import Arduino, util
 import serial
 import time
-from multiprocessing import Process, Lock
+from multiprocessing import Process, Lock, Event
 from multiprocessing.sharedctypes import Array
 from ctypes import Structure, c_long
 import re
+
+
+class MotorProcess(Process):
+    def __init__(self, control_funct, location):
+        Process.__init__(self)
+        self.exit = Event()
+        self.control_funct = control_funct
+        self.location = location
+
+    def shutdown(self):
+        self.exit.set()
+
+    def run(self):
+        while not self.exit.is_set():
+            self.control_funct(self.location)
 
 
 class ArduControl():
@@ -155,74 +170,70 @@ class ArduControl():
 
         if motorA:
             if self._motorA_pos_process:
-                self._motorA_pos_process.terminate()
+                self._motorA_pos_process.shutdown()
                 while self._motorA_pos_process.is_alive():
                     pass
-            self._motorA_pos_process = Process(target=self.motorAWorker,
-                                               args=(motorA,))
+            self._motorA_pos_process = MotorProcess(self.motorAWorker,
+                                                    motorA)
             self._motorA_pos_process.start()
         if motorB:
             if self._motorB_pos_process:
-                self._motorB_pos_process.terminate()
+                self._motorB_pos_process.shutdown()
                 while self._motorB_pos_process.is_alive():
                     pass
-            self._motorB_pos_process = Process(target=self.motorBWorker,
-                                               args=(motorB,))
+            self._motorB_pos_process = MotorProcess(self.motorBWorker,
+                                                    motorB)
             self._motorB_pos_process.start()
 
     def motorAWorker(self, goto_pos):
         """
         Worker function for process gotoPosition
         """
-        while True:
-            phy_pos = (self.encoder.getPositions()[0])
-            self.motorDirection(motorA=int(goto_pos < phy_pos))
-            p_vel = self.p_gain * ((goto_pos[0]*464 + goto_pos[1]) -
-                                   (phy_pos[0]*464 + phy_pos[1]))
-            d_vel = self.d_gain * self.motorA_speed.read()
-            if self.motorA_direction.read() == 1:
-                d_vel = -d_vel
-            vel = p_vel - d_vel
+        phy_pos = (self.encoder.getPositions()[0])
+        self.motorDirection(motorA=int(goto_pos < phy_pos))
+        p_vel = self.p_gain * ((goto_pos[0]*464 + goto_pos[1]) -
+                               (phy_pos[0]*464 + phy_pos[1]))
+        d_vel = self.d_gain * self.motorA_speed.read()
+        if self.motorA_direction.read() == 1:
+            d_vel = -d_vel
+        vel = p_vel - d_vel
 
-            if vel == abs(vel):
-                self.motorDirection(0)
-            else:
-                self.motorDirection(1)
-            vel = abs(vel)
+        if vel == abs(vel):
+            self.motorDirection(0)
+        else:
+            self.motorDirection(1)
+        vel = abs(vel)
 
-            if vel > 1:
-                vel = 1
+        if vel > 1:
+            vel = 1
 
-            if self.motorA_speed.read() != vel:
-                self.motorSpeed(motorA=vel)
-        self._motorA_newpos.clear()
+        if self.motorA_speed.read() != vel:
+            self.motorSpeed(motorA=vel)
 
     def motorBWorker(self, goto_pos):
         """
         Worker function as a target for the motorB part of gotoPosition
         """
-        while True:
-            phy_pos = (self.encoder.getPositions()[1])
-            self.motorDirection(motorA=int(goto_pos < phy_pos))
-            p_vel = self.p_gain * ((goto_pos[0]*464 + goto_pos[1]) -
-                                   (phy_pos[0]*464 + phy_pos[1]))
-            d_vel = self.d_gain * self.motorB_speed.read()
-            if self.motorB_direction.read() == 1:
-                d_vel = -d_vel
-            vel = p_vel - d_vel
+        phy_pos = (self.encoder.getPositions()[1])
+        self.motorDirection(motorA=int(goto_pos < phy_pos))
+        p_vel = self.p_gain * ((goto_pos[0]*464 + goto_pos[1]) -
+                               (phy_pos[0]*464 + phy_pos[1]))
+        d_vel = self.d_gain * self.motorB_speed.read()
+        if self.motorB_direction.read() == 1:
+            d_vel = -d_vel
+        vel = p_vel - d_vel
 
-            if vel == abs(vel):
-                self.motorDirection(motorB=0)
-            else:
-                self.motorDirection(motorB=1)
-            vel = abs(vel)
+        if vel == abs(vel):
+            self.motorDirection(motorB=0)
+        else:
+            self.motorDirection(motorB=1)
+        vel = abs(vel)
 
-            if vel > 1:
-                vel = 1
+        if vel > 1:
+            vel = 1
 
-            if self.motorB_speed.read() != vel:
-                self.motorSpeed(motorB=vel)
-        self._motorB_newpos.clear()
+        if self.motorB_speed.read() != vel:
+            self.motorSpeed(motorB=vel)
 
 
 class Position(Structure):
