7a7fa1e6c7487af70bcf0d556319193eeec0460b
Joshua Deare <doodles@doodles.(none)>
2013-06-02 22:53:05 -0700

    changed to all general code to encoder0

--- Encoder_Interupt_Revs/Encoder_Interupt_Revs.ino
+++ Encoder_Interupt_Revs/Encoder_Interupt_Revs.ino
@@ -4,12 +4,13 @@
 
 
 volatile int encoder0Pos = 0;
-volatile boolean PastA = 0;
-volatile boolean PastB = 0;
-int revolutions = 0;
+volatile boolean PastA0 = 0;
+volatile boolean PastB0 = 0;
+int revolutions0 = 0;
 
-int lastEncode = 1;
-int lastRevs = 1;
+
+int lastEncode0 = 1;
+int lastRevs0 = 1;
 volatile unsigned int timeout = 0;
 
 void setup() 
@@ -21,14 +22,14 @@ void setup()
   pinMode(encoder0PinB, INPUT); 
   //turn on pullup resistor
   //digitalWrite(encoder0PinB, HIGH); //ONLY FOR SOME ENCODER(MAGNETIC)!!!! 
-  PastA = (boolean)digitalRead(encoder0PinA); //initial value of channel A;
-  PastB = (boolean)digitalRead(encoder0PinB); //and channel B
+  PastA0 = (boolean)digitalRead(encoder0PinA); //initial value of channel A;
+  PastB0 = (boolean)digitalRead(encoder0PinB); //and channel B
 
 //To speed up even more, you may define manually the ISRs
 // encoder A channel on interrupt 0 (arduino's pin 2)
-  attachInterrupt(0, doEncoderA, RISING);
+  attachInterrupt(0, doEncoderA0, RISING);
 // encoder B channel pin on interrupt 1 (arduino's pin 3)
-  attachInterrupt(1, doEncoderB, CHANGE); 
+  attachInterrupt(1, doEncoderB0, CHANGE); 
   Serial.begin(9600);
 }
 
@@ -36,11 +37,11 @@ void setup()
 void loop()
 {  
  //your staff....ENJOY! :D
- if(revolutions != lastRevs || encoder0Pos != lastEncode || timeout > 60000)  {
+ if(revolutions0 != lastRevs0 || encoder0Pos != lastEncode0 || timeout > 60000)  {
    timeout = 0;
-   lastRevs = revolutions;
-   lastEncode= encoder0Pos;
-   Serial.print(String(String(revolutions, DEC) + String(",") + String(encoder0Pos, DEC) + String(";\n")));
+   lastRevs0 = revolutions0;
+   lastEncode0 = encoder0Pos;
+   Serial.print(String(String(revolutions0, DEC) + String(",") + String(encoder0Pos, DEC) + String(";\n")));
    //Serial.print(",");
    //Serial.print(encoder0Pos, DEC);
    //Serial.print(";\n");
@@ -51,20 +52,20 @@ void loop()
 
 //you may easily modify the code  get quadrature..
 //..but be sure this whouldn't let Arduino back! 
-void doEncoderA()
+void doEncoderA0()
 {
-     PastB ? encoder0Pos--:  encoder0Pos++;
+     PastB0 ? encoder0Pos--:  encoder0Pos++;
      if (encoder0Pos > 464 || encoder0Pos < -464)  {
-       revolutions += encoder0Pos / 464;
+       revolutions0 += encoder0Pos / 464;
        encoder0Pos %= 464;
      }
      
-     if (revolutions > 0 && encoder0Pos < 0 )  {
-       revolutions --;
+     if (revolutions0 > 0 && encoder0Pos < 0 )  {
+       revolutions0 --;
        encoder0Pos = 464 - encoder0Pos;
      }
-     else if (revolutions < 0 && encoder0Pos > 0)  {
-       revolutions ++;
+     else if (revolutions0 < 0 && encoder0Pos > 0)  {
+       revolutions0 ++;
        encoder0Pos = -464 - encoder0Pos;
      }
        
@@ -72,7 +73,7 @@ void doEncoderA()
      
 }
 
-void doEncoderB()
+void doEncoderB0()
 {
-     PastB = !PastB; 
+     PastB0 = !PastB0; 
 }
